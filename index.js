f20c0719c68b030d05c4484827b95789a41cb615
slyguy <theslyguy@icloud.com>
2016-11-24 07:12:42 +0800

    refactored into separate module

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var http = require("http"), express = require("express");
-var urlShorter = require('node-url-shorter');
-var tinyURL = require('tinyurl');
-var shorten = require('simple-short');
-var gglurl = require('node-gglurl');
+var shorten = require('./sg-shorten');
 
 var app = express();
 
@@ -12,47 +9,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/new/:url(*)", function(req,res){
     res.writeHead(200);
-    var url = req.params.url;
-    let responseObj = {};
-    //var valid = /^(ftp|http|https):\/\/[^ "]+$/.test(url);
-    var re = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/; ///^(http|https):\/\/[^ "]+$/
-    var valid = re.test(url);
-    if(valid){
-      tinyURL.shorten(url, function(s_url) {
-        console.log(s_url);
-        responseObj = {original_url: url, short_url: s_url };
-        res.end(JSON.stringify(responseObj, null, " "));
-      });
-      /*urlShorter.getShortUrl(url)
-      .then(function(s_url){
-          responseObj = {original_url: url, short_url: s_url };
-          res.end(JSON.stringify(responseObj, null, " "));
-      })
-      .fail(function(err){
-        responseObj = {error:err };
-        res.end(JSON.stringify(responseObj, null, " "));
-      });
-      shorten(url, function(s_url) {
-        console.log(s_url);
-        responseObj = {original_url: url, short_url: s_url };
-        res.end(JSON.stringify(responseObj, null, " "));
-      });
-      gglurl.encode(function(err, s_url) {
-        if (err) {
-            console.log(err);
-            responseObj = {error:err };
-            res.end(JSON.stringify(responseObj, null, " "));
-        } else {
-            console.log(s_url);
-            responseObj = {original_url: url, short_url: s_url };
-            res.end(JSON.stringify(responseObj, null, " "));
-        }
-      }, url);*/
-    }
-    else{
-      responseObj = {error: "Invalid url" };
-      res.end(JSON.stringify(responseObj, null, " "));
-    }
+    res.end(shorten(req.params.url));
 });
 
 app.get("/", function(req,res){
